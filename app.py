c02789d1f259b48981f7eebec8793bc77183786a
黃士榮 <o935096394@gmail.com>
2025-07-12 00:44:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,45 @@
 import os
 from flask import Flask, request, abort
-from linebot.v3 import WebhookHandler
-from linebot.v3.messaging import MessagingApi, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3 import WebhookParser
 from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import MessagingApiClient, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
-CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
+CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
-handler = WebhookHandler(CHANNEL_SECRET)
-messaging_api = MessagingApi(CHANNEL_ACCESS_TOKEN)
+if not CHANNEL_ACCESS_TOKEN or not CHANNEL_SECRET:
+    raise Exception("Missing CHANNEL_ACCESS_TOKEN or CHANNEL_SECRET!")
 
-@app.route("/", methods=['GET'])
-def index():
-    return "LINE Bot is running."
+line_bot_api = MessagingApiClient(CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers.get('X-Line-Signature')
+    signature = request.headers.get('X-Line-Signature', '')
     body = request.get_data(as_text=True)
 
     try:
-        handler.handle(body, signature)
+        events = parser.parse(body, signature)
     except InvalidSignatureError:
         abort(400)
 
+    for event in events:
+        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessageContent):
+            line_bot_api.reply_message(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=f"你剛剛說了：{event.message.text}")]
+                )
+            )
+
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    received_text = event.message.text
-    reply_message = TextMessage(text=f'你說的是：{received_text}')
-    messaging_api.reply_message(
-        event.reply_token,
-        [reply_message]
-    )
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
+
