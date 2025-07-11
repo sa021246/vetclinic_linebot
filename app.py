5c2909631fb44cd0b029b4487dc5db4dd26abc82
黃士榮 <o935096394@gmail.com>
2025-07-12 00:31:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,45 @@
 import os
 from flask import Flask, request, abort
-from linebot.v3 import WebhookParser
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import MessagingApiClient, TextMessage
+from linebot.v3 import WebhookHandler
+from linebot.v3.messaging import MessagingApi, TextMessage
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from linebot.v3.exceptions import InvalidSignatureError
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
+CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
 
-parser = WebhookParser(LINE_CHANNEL_SECRET)
-client = MessagingApiClient(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+messaging_api = MessagingApi(CHANNEL_ACCESS_TOKEN)
 
+@app.route("/", methods=['GET'])
+def index():
+    return "LINE Bot is running."
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers.get('X-Line-Signature', '')
+    signature = request.headers.get('X-Line-Signature')
     body = request.get_data(as_text=True)
 
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
-    for event in events:
-        if isinstance(event, MessageEvent) and isinstance(event.message, TextMessageContent):
-            handle_message(event)
-
     return 'OK'
 
-
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    user_id = event.source.user_id
     received_text = event.message.text
-
-    reply_text = f"你說的是：{received_text}"
-
-    client.reply_message(
+    reply_message = TextMessage(text=f'你說的是：{received_text}')
+    messaging_api.reply_message(
         event.reply_token,
-        [
-            TextMessage(text=reply_text)
-        ]
+        [reply_message]
     )
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
